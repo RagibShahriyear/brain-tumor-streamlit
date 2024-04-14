f739c144fa7a7ec72749847d68082396becc4341
Ragib Shahriyear <18-36250-1@student.aiub.edu>
2024-04-15 01:28:16 +0600

    cleared clutted and made it better.

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-
+import os
+from keras.models import load_model
+from main import RetPreds
 
 # File Processing Pkgs
 from PIL import Image
+
+model = load_model("trained_model.h5")
 
 
 # Load Images
@@ -14,36 +17,39 @@ def load_image(image_file):
 
 
 def main():
-    st.title("File Uplaod Tutorial")
+    st.title("Brain Tumor Detection System")
 
-    menu = ["Home", "Dataset", "DocumentFiles", "About"]
+    menu = ["Home", "About"]
+
     choice = st.sidebar.selectbox("Menu", menu)
 
     if choice == "Home":
-        st.subheader("Home")
-        image_file = st.file_uploader("Upload Images", type=["png", "jpg", "jpeg"])
-
+        st.subheader("Upload a brain MRI image for prediction")
+        image_file = st.file_uploader("Upload an Image", type=["jpg"])
         if image_file is not None:
-            # To see details
-            st.write(type(image_file))
-            st.write(dir(image_file))
-            file_details = {
-                "filename": image_file.name,
-                "filetype": image_file.type,
-                "filesize": image_file.size,
-            }
-            st.write(file_details)
+            # file_details = {"FileName": image_file.name, "FileType": image_file.type}
+            # st.write(file_details)
+            # st.write(dir(image_file))
+            # st.write(type(image_file))
+            img = load_image(image_file)
+            st.image(img)
+            # saving file (needed for making prediction)
+            with open(os.path.join("uploaded_test", image_file.name), "wb") as f:
+                f.write(image_file.getbuffer())
 
-            st.image(load_image(image_file))
+            # st.success("File Saved")
+            prediction = RetPreds(model)
+            st.write("Looks like this MRI image is:", prediction[0])
+            # st.write(prediction[0])
 
-    elif choice == "Dataset":
-        st.subheader("Dataset")
-
-    elif choice == "DocumentFiles":
-        st.subheader("DocumentFiles")
+            path = os.path.join("uploaded_test/", image_file.name)
+            os.remove(path)
 
     else:
         st.subheader("About")
+        st.write(
+            "Welcome to our Brain MRI Predictor! This Streamlit app utilizes cutting-edge Convolutional Neural Network (CNN) technology to analyze brain MRI images and predict the presence of abnormalities with an impressive 98% accuracy rate. Simply upload your MRI image, and our powerful model will swiftly process it, providing you with insights into the presence of any anomalies. Whether you're a healthcare professional or an individual concerned about your brain health, our app offers a reliable and accessible tool for early detection and diagnosis."
+        )
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,86 +1,11 @@
-import os
 import pandas as pd
-import tensorflow as tf
+
 from pathlib import Path
 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from keras.models import load_model
 
 model = load_model("trained_model.h5")
-
-
-# new_test_path = Path("test/")
-
-# new_test_jpg = list(new_test_path.glob(r"*.jpg"))
-
-# print(new_test_jpg)
-
-
-# def get_jpg_label(x):
-#     return os.path.split(os.path.split(x)[0])[1]
-
-
-# ragib_test_JPG_labels = list(map(get_jpg_label, new_test_jpg))
-# print(ragib_test_JPG_labels)
-
-# Ragib_Test_JPG_Path_Series = pd.Series(new_test_jpg, name="JPG").astype(str)
-# Ragib_Test_JPG_Labels_Series = pd.Series(ragib_test_JPG_labels, name="TUMOR_CATEGORY")
-
-# Test_Ragib = pd.concat(
-#     [Ragib_Test_JPG_Path_Series, Ragib_Test_JPG_Labels_Series], axis=1
-# )
-
-# print(Test_Ragib)
-
-# figure = plt.figure(figsize=(5, 5))
-# plt.imshow(plt.imread(Test_Ragib["JPG"][3]))
-
-# Generator_Basic = ImageDataGenerator(rescale=1.0 / 255, validation_split=0.1)
-
-# Test_Set_2 = Generator_Basic.flow_from_dataframe(
-#     dataframe=Test_Ragib,
-#     x_col="JPG",
-#     y_col="TUMOR_CATEGORY",
-#     color_mode="grayscale",
-#     class_mode="categorical",
-#     target_size=(200, 200),
-# )
-
-# # for data_batch, label_batch in Test_Set_2:
-# #     print("data shape: ", data_batch.shape)
-# #     print("label shape: ", label_batch.shape)
-# #     break
-
-# # print(Test_Set_2.class_indices)
-# # print(Test_Set_2.classes)
-# # print(Test_Set_2.image_shape)
-
-# Ragib_Data_Prediction = pd.DataFrame({"JPG": Ragib_Test_JPG_Path_Series})
-
-# # print(Ragib_Data_Prediction)
-
-# Main_Test_Generator = ImageDataGenerator(rescale=1.0 / 255)
-
-# Ragibs_Test_Set = Main_Test_Generator.flow_from_dataframe(
-#     dataframe=Ragib_Data_Prediction,
-#     x_col="JPG",
-#     y_col=None,
-#     color_mode="grayscale",
-#     class_mode=None,
-#     target_size=(200, 200),
-# )
-
-# Ragib_Test_Prediction = model.predict(Ragibs_Test_Set)
-
-# Ragib_Test_Prediction = Ragib_Test_Prediction.argmax(axis=-1)
-# print(Ragib_Test_Prediction)
-
-# Last_Prediction = []
-# [
-#     Last_Prediction.append("NO") if i == 1 else Last_Prediction.append("TUMOR")
-#     for i in Ragib_Test_Prediction
-# ]
-# print(Last_Prediction)
 
 
 def RetPreds(model):
